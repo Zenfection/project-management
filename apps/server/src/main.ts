9b3b4a7ed3733a87235a8d414e038d70f25ad02d
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-12-02 10:41:53 +0700

    update

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,6 +11,13 @@ import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
