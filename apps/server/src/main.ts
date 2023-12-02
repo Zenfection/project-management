1a154cfdc52b6e6da865c710b8bbda647d54713b
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-12-02 10:50:45 +0700

    Update main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://ctu-task.zenfection.com'],
+    origin: '*',
     allowedHeaders: '*',
     methods: '*',
     credentials: true,
