d0db19a37fb2bcb1a036ae480399b8889a7f3e5c
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-12-02 11:07:24 +0700

    Update main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
